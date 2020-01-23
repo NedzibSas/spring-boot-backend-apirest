a29adaf009c7a4c56d934cbae40cc366dd10eb5d
Brayan Nedzib Sastoque Rangel <nedzib.sastoque@gmail.com>
2020-01-23 13:50:12 -0500

    add Cross for front permissions

--- src/main/java/com/bolsadeideas/springboot/backend/apirest/controllers/ClienteRestController.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -3,6 +3,7 @@ package com.bolsadeideas.springboot.backend.apirest.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.bolsadeideas.springboot.backend.apirest.models.entity.Cliente;
 import com.bolsadeideas.springboot.backend.apirest.models.services.iClienteService;
 
+@CrossOrigin(origins = { "http://localhost:4200" }) // configuracion para saber que peticiones son permitidas por el
+													// back en el lado del cliente
 @RestController // marcar la clase como rest controller
 @RequestMapping("/api") // mapear el rest controller el end point de la url
 public class ClienteRestController {
